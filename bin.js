c4a4b1921bdb2732b05304b8f1cf82bef9e3261d
jrucker <1435255141@qq.com>
2018-05-17 23:56:36 +0800

    fix message

--- bin.js
+++ bin.js
@@ -2,9 +2,9 @@
 const program = require('commander')
 const translate = require('./translator')
 program.version(require('./package.json').version)
-  .action((arg,cmd)=>{
-    translate(arg).then((res=[])=>{
-      res.forEach(r=>console.log(r.replace(new RegExp('；'),'')))
+    .action((arg, cmd) => {
+        translate(arg).then((res = []) => {
+            res.forEach(r => console.log(r.replace(new RegExp('；'), '')))
+        })
     })
-  })
-  .parse(process.argv)
+    .parse(process.argv)
